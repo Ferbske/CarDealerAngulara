775760ebd572e1db14dc863baa4736dd328258e0
Unknown <rick.voesenek@hotmail.com>
2018-12-06 16:12:17 +0100

    Started getting info from heruko api

--- src/app/components/car-list/car-list.component.ts
+++ src/app/components/car-list/car-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { CarModel } from '../../models/car/car.model';
 import { CarService } from '../../models/car/car.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-car-list',
@@ -8,10 +9,13 @@ import { CarService } from '../../models/car/car.service';
   styleUrls: ['./car-list.component.css']
 })
 export class CarListComponent implements OnInit {
+  title = 'Car List Component';
+  cars$: Observable<CarModel[]>;
   cars: CarModel[];
 
   constructor(private carService: CarService) {
-    this.cars = carService.getAllCars();
+    // this.cars$ = this.carService.getAllCars();
+    this.cars = carService.allCars();
   }
 
   ngOnInit() {
--- src/app/models/car/car.service.ts
+++ src/app/models/car/car.service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {CarModel} from './car.model';
+import {Observable} from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +16,19 @@ export class CarService {
     new CarModel(1237,'Test Car 4','Test Car 4','Test Car 4')
   ];
 
+  // constructor(private http: HttpClient) { }
   constructor() { }
 
-  getAllCars(): CarModel[] {
+  allCars(): CarModel[] {
     return this.cars;
   }
+
+  // getAllCars(): Observable<CarModel[]> {
+    // return this.http.get<any>('https://cardealer-api.herokuapp.com/car')
+    //   .pipe(
+    //     map(result => result.results)
+    //   );
+  // }
 
   findOneCar(index: number): CarModel {
     return this.cars[index];
